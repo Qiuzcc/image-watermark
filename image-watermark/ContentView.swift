0aa9214f995afd6bebaf771dc3a1235d77ef16e9
qiuzcc <qiuzcc@qq.com>
2026-05-07 11:33:33 +0800

    Initial Commit

---
+++ image-watermark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  image-watermark
+//
+//  Created by timegogo on 2026/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ image-watermark/image_watermarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_watermarkApp.swift
+//  image-watermark
+//
+//  Created by timegogo on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct image_watermarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
